6058d4ae61aaebd52f622025334b679cd45797b4
Gabe-Lee <gla.ander@gmail.com>
2020-01-10 08:23:04 -0600

    Change url string

--- env.js
+++ env.js
@@ -2,8 +2,8 @@ module.exports.local = {
   env: {
     PORT: 5555,
     HOST: 'localhost',
-    URL: process.env.NODE_ENV === 'production' ? 'https://team-sweeper.herokuapp.com' : 'http://localhost:5555',
-    SOCKET: process.env.NODE_ENV === 'production' ? 'wss://team-sweeper.herokuapp.com' : 'ws://localhost:5555',
+    URL: process.env.PORT ? 'https://team-sweeper.herokuapp.com' : 'http://localhost:5555',
+    SOCKET: process.env.PORT ? 'wss://team-sweeper.herokuapp.com' : 'ws://localhost:5555',
   },
 };
 
